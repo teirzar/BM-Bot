a5eaff78cc4be2ad1a2475f41fd26153220d554d
teirzar <nik.ul@icloud.com>
2023-07-07 21:15:26 +0300

    исправлено: keyborads/__init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,1 @@
-from client_kb import kb_client_main_menu, kb_client_settings_menu, kb_client_menu_menu
+from keyboards.client_kb import kb_client_main_menu, kb_client_settings_menu, kb_client_menu_menu
