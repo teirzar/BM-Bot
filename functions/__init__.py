810728cecfd2fd36a6ce3bbbed9816285662aaea
teirzar <nik.ul@icloud.com>
2023-07-07 21:14:33 +0300

    изменено: functions/__init__.py, functions/client_functions.py
    Добавлена функция get_admins, которая возвращает кортеж с админами, исправлен functions/__init__.py (внесено get_admins)

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,2 +1,2 @@
 from functions.other_functions import get_time, get_token, add_log
-from functions.client_functions import get_tg_id, get_user_id
+from functions.client_functions import get_tg_id, get_user_id, get_admins
--- functions/client_functions.py
+++ functions/client_functions.py
@@ -13,3 +13,12 @@ async def get_user_id(message: types.Message | types.CallbackQuery) -> int:
     """Возвращает id пользователя из базы данных"""
     return int(users.print_table('id', where=f"tg_id = {await get_tg_id(message)}")[0][0])
 
+
+# get admins
+async def get_admins() -> tuple:
+    """Возвращает кортеж из администраторов бота"""
+    admins = users.print_table('tg_id', where='status = 99')
+    if admins:
+        tpl_out = (el[0] for el in admins)
+        return tuple(tpl_out)
+    return tuple()
